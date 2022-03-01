1e20332547bc783c72d2f0b8a00444ff813f1fda
Gurianov-syd <82145284+Gurianov-syd@users.noreply.github.com>
2022-03-01 13:55:28 +0300

    Add files via upload

---
+++ www/111/RowTable.swift
@@ -0,0 +1,15 @@
+//
+//  RowTable.swift
+//
+//
+//  Created by Student on 01.03.2022.
+//
+
+import WatchKit
+
+class RowTable: NSObject {
+    
+    @IBOutlet weak var rowPicture: WKInterfaceImage!
+    @IBOutlet weak var rowRecipeName: WKInterfaceLabel!
+
+}
